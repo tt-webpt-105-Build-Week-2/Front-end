175e37eb738092b0aa0ee6c6e8d586db94744a84
Clare Jackson <clare-jackson@lambdastudents.com>
2020-11-12 20:42:02 -0500

    LoginForm working. No styling.

--- secret-family-recipe/src/Login/LoginForm.js
+++ secret-family-recipe/src/Login/LoginForm.js
@@ -41,31 +41,92 @@ const LoginForm = () => {
   }
     const validate = (name, value) => {
         //yup validation schema
+        yup
+          .reach(schema, name)
+          .validate(value)
+          .then(valid => {
+            setFormErrors({
+              ...formErrors,
+              [name]: ""
+            })
+          })
+          .catch(error => {
+           setFormErrors({
+             ...formErrors,
+              [name]: error.errors[0]
+          })
+         })
     }
 
-    const inputChange = (name, value) => {
+    const onChange =  event => {
+      const { name, value } = event.target
+      change(name, value)
+  }
+
+    const change = (name, value) => {
       validate(name, value)
       setFormValues({
         ...formValues,
         [name]: value
-      })
-    }
+    })
+  }
 
-    const formSubmit = () => {
+    const onSubmit = event => {
+      event.preventDefault()
+      submit()
+  }
+
+    const submit = () => {
       const newUser = {
         email: formValues.email.trim(),
         password: formValues.password.trim()
-      }
-      postNewUsers(newUser)
     }
-  
+      postNewUsers(newUser)
+  }
 
+    useEffect(() => {
+      console.log(formValues)
+   }, [formValues])
+
+    useEffect(() => {
+      schema.isValid(formValues)
+        .then(valid => {
+         setDisabled(!valid)
+       })
+    }, [formValues])
 
 
 
 
   return (
-    null
+    <form onSubmit={onSubmit}>
+      <h1>Login Form</h1>
+
+        <div> 
+          <div>{formErrors.name}</div>
+          <div>{formErrors.email}</div>
+          <div>{formErrors.password}</div>
+          <div>{formErrors.terms}</div>
+        </div>
+
+        <label>Email:     </label>
+        <input 
+        value={formValues.email}
+        onChange={onChange}
+        name='email'
+        type='email'
+        />
+
+        <label>Password:     </label>
+        <input 
+        value={formValues.password}
+        onChange={onChange}
+        name='password'
+        type='password'
+        />
+
+      <button disabled={disabled} id='submitBtn'>Submit</button>
+    </form>
   )
 }
 
