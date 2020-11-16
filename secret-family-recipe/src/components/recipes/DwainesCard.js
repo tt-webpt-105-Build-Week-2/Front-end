f0acfdb8fb3bd44c2f84854273c93481ff5058ad
dwainejade <dwainejade@gmail.com>
2020-11-15 21:16:11 -0500

    styling recipe card

--- secret-family-recipe/src/components/recipes/DwainesCard.js
+++ secret-family-recipe/src/components/recipes/DwainesCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap';
+import './card.css'
 
 const DwainesCard = ({ title, source, id }) => {
     return (
         <div>
-            <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src="https://loremflickr.com/640/360" />
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" className="card-img" src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/3c0710e0531b41b7804acf4a52ee15be/FB_04forpkg.jpg?output-format=auto&output-quality=auto" />
                 <Card.Body>
-                    <Card.Title >Recipe Title</Card.Title>
+                    <Card.Title >Creamy Mac 'n' Cheese</Card.Title>
                     <Card.Text>
                         Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage chicken.
                     </Card.Text>
