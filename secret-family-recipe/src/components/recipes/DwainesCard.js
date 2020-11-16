3d023275cb3b9ca95e9acbfa69e6ccc9ae7db9cb
dwainejade <dwainejade@gmail.com>
2020-11-15 21:58:13 -0500

    added zoom to img

--- secret-family-recipe/src/components/recipes/DwainesCard.js
+++ secret-family-recipe/src/components/recipes/DwainesCard.js
@@ -5,7 +5,7 @@ import './card.css'
 const DwainesCard = ({ title, source, id }) => {
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
+            <Card className="card-container" style={{ width: '18rem' }}>
                 <Card.Img variant="top" className="card-img" src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/3c0710e0531b41b7804acf4a52ee15be/FB_04forpkg.jpg?output-format=auto&output-quality=auto" />
                 <Card.Body>
                     <Card.Title >Creamy Mac 'n' Cheese</Card.Title>
