25cbc55b455e79c60efc095e40cc7a93e7c0c09d
Jonah D. Hadlock <64397503+jonahdhadlock@users.noreply.github.com>
2020-11-17 19:11:02 -0700

    Add files via upload
    updates not complete yet

--- secret-family-recipe/src/Login/LoginForm.js
+++ secret-family-recipe/src/Login/LoginForm.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './LoginForm.less';
+import './LoginForm.css';
 import schema from './loginSchema'
-import  * as yup from 'yup'
+import * as yup from 'yup'
 import { Route, Switch } from 'react-router-dom'
 import axios from 'axios'
+import { useHistory } from "react-router-dom";
 
 const initialFormValues = {
   email: '',
@@ -16,9 +17,6 @@ const initialFormErrors = {
 const initialUsers = []
 const initialDisabled = true
 
-
-
-
 const LoginForm = () => {
 
   const [users, setUsers] = useState(initialUsers)
@@ -30,6 +28,8 @@ const LoginForm = () => {
     axios.post('https://reqres.in/api/users', newUser)
       .then(response => {
         setUsers([...users, response.data])
+        localStorage.setItem("token", response.data.payload);
+        window.location.assign('/recipes');
       })
       .catch(error => {
         // debugger 
@@ -39,94 +39,98 @@ const LoginForm = () => {
         setFormValues(initialFormValues)
       })
   }
-    const validate = (name, value) => {
-        //yup validation schema
-        yup
-          .reach(schema, name)
-          .validate(value)
-          .then(valid => {
-            setFormErrors({
-              ...formErrors,
-              [name]: ""
-            })
-          })
-          .catch(error => {
-           setFormErrors({
-             ...formErrors,
-              [name]: error.errors[0]
-          })
-         })
-    }
-
-    const onChange =  event => {
-      const { name, value } = event.target
-      change(name, value)
+  const validate = (name, value) => {
+    //yup validation schema
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ""
+        })
+      })
+      .catch(error => {
+        setFormErrors({
+          ...formErrors,
+          [name]: error.errors[0]
+        })
+      })
   }
 
-    const change = (name, value) => {
-      validate(name, value)
-      setFormValues({
-        ...formValues,
-        [name]: value
+  const onChange = event => {
+    const { name, value } = event.target
+    change(name, value)
+  }
+
+  const change = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value
     })
   }
 
     const onSubmit = event => {
       event.preventDefault()
       submit()
+      // change route to home page, dashboard
   }
 
-    const submit = () => {
-      const newUser = {
-        email: formValues.email.trim(),
-        password: formValues.password.trim()
+  const submit = () => {
+    const newUser = {
+      email: formValues.email.trim(),
+      password: formValues.password.trim()
     }
-      postNewUsers(newUser)
+    postNewUsers(newUser)
   }
 
-    useEffect(() => {
-      console.log(formValues)
-   }, [formValues])
+  useEffect(() => {
+    console.log(formValues)
+  }, [formValues])
 
-    useEffect(() => {
-      schema.isValid(formValues)
-        .then(valid => {
-         setDisabled(!valid)
-       })
-    }, [formValues])
+  useEffect(() => {
+    schema.isValid(formValues)
+      .then(valid => {
+        setDisabled(!valid)
+      })
+  }, [formValues])
 
 
 
 
   return (
+    <div>
     <form onSubmit={onSubmit}>
-      <h1>Login Form</h1>
+      <h1>Login</h1>
 
-        <div> 
-          <div>{formErrors.name}</div>
-          <div>{formErrors.email}</div>
-          <div>{formErrors.password}</div>
-          <div>{formErrors.terms}</div>
-        </div>
+      <div>
+        <div>{formErrors.name}</div>
+        <div>{formErrors.email}</div>
+        <div>{formErrors.password}</div>
+        <div>{formErrors.terms}</div>
+      </div>
 
-        <label>Email:     </label>
-        <input 
+      <label>Email:     </label>
+      <input
         value={formValues.email}
         onChange={onChange}
         name='email'
         type='email'
-        />
-        <br></br>
-        <label>Password:     </label>
-        <input 
+      />
+
+      <label>Password:     </label>
+      <input
         value={formValues.password}
         onChange={onChange}
         name='password'
         type='password'
-        />
+      />
 
       <button disabled={disabled} id='submitBtn'>Submit</button>
     </form>
+    {/* new users click here, or something else, need to sign up? */}
+    </div>
   )
 }
 
